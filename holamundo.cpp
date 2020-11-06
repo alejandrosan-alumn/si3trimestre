8b85e8bd7639749a97da4ad429dac03316960f35
Lex <alejandrosan.alumn@cescristorey.com>
2020-11-06 14:31:02 +0100

    holamundov2

--- holamundo.cpp
+++ holamundo.cpp
@@ -3,9 +3,18 @@
 
 using manespace std;
 
+int hola(int hola){
+
+	cout << "Hola mundo" << endl;
+	return hola;
+
+}
+
 int main (){
 
-	string hola=" ";
-	cout << "Hola" << hola << "mundo" << endl;
+	string hola = NULL;
+	int holaa = 9;
+	holaa= hola(holaa);
+	
 return 0;
 }
